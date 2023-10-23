a0d747dba4e3e6413551bf1f819be3e6e861e6e0
Julio <55517364+GONZALEZ-RODRIGUES@users.noreply.github.com>
2023-10-23 19:59:40 +0900

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = 8000;
 
 app.get("/", userController.getUsers);
 app.get("/:id", userController.getUsers);
